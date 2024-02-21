83887077c476b0053dda56d39e822123cc42be12
Spas012 <longlivechoice@gmail.com>
2024-02-21 17:07:07 -0300

    pepe8

--- Advanced calculator.py
+++ Advanced calculator.py
@@ -1,6 +1,7 @@
- # This allows the user to choose how many terms to use in the operations
+# This allows the user to choose how many terms to use in the operations
 import math
 import cmath
+
 
 def Addition(x, y):
     return (x + y)
@@ -18,32 +19,38 @@ def Division(x, y):
     except ZeroDivisionError:
         return "Undefined"
 
+def Exponents(x, y):
+    return (x**y)
+
 def SquareRoot(x):
     return (cmath.sqrt(x))
 
 def factors(x):
     neg = False
+
     if x < 0:
         neg = True
         x = -x
+
     factors_list = [1, x]
     complete_list = []
+
     for i in range(2, math.ceil(math.sqrt(x) + 1)):
         if x % i == 0:
-            factors_list += (i, x//i)
+            factors_list += (i, x // i)
+
     if neg:
         complete_list = factors_list + list(reversed(factors_list))
+
     else:
         complete_list = factors_list
 
     return complete_list
-    
 
-def Exponents(x, y):
-    return (x **y)
+def QuadraticFormula(x, y, z):
+    return ((-y + cmath.sqrt(y**2 - 4*x*z)) / (2*x)) ,\
+    ((-y - cmath.sqrt(y**2 - 4*x*z)) / (2*x))
 
-def QuadraticFormula(x,y,z):
-    return ((-y + cmath.sqrt(y**2 - 4*x*z))/(2*x)) , ((-y - cmath.sqrt(y**2 - 4*x*z))/(2*x))
 
 print("Select operation.")
 print(" 1. Addition")
@@ -64,8 +71,7 @@ a = 0
 b = 0
 c = 0
 
-while True: 
-    # Take user input
+while True:
     choice = input("Enter choice (1, 2, 3, 4, 5, 6, 7, or 8): ")
 
     # checks if choice is one of the six options
@@ -74,35 +80,48 @@ while True:
           num1 = int(input("Enter number: "))
           if choice in ('1', '2', '3', '4', '7'):
               num2 = int(input("Enter second number: "))
+
         except ValueError:
             print("Invalid input. Please enter a number")
             continue
+
         if choice == '1':
             print(num1, "+", num2, "=", Addition(num1, num2))
+
         elif choice == '2':
             print(num1, "-", num2, "=", Subtraction(num1, num2))
+
         elif choice == '3':
             print(num1, "*", num2, "=", Multiplication(num1, num2))
+
         elif choice == '4':
             print(num1, "/", num2, "=", Division(num1, num2))
+
         elif choice == '5':
             print(cmath.sqrt(num1))
+
         elif choice == '6':
             print(f"The factors of {num1} are: ")
-
             res = factors(num1)
+
             for i in range(0, len(res), 2):
                 print(res[i], "\t\t", res[i+1])
+
         else:
-            print(num1,"^", num2, "=", Exponents(num1, num2))
+            print(num1, "^", num2, "=", Exponent(num1, num2))
+
     if choice == '8':
         try:
             num1 = int(input("Enter your a value: "))
+
             num2 = int(input("Enter your b value: "))
+
             num3 = int(input("Enter your c value: "))
+
         except ValueError:
             print("Invalid input. Please enter a number")
             continue
+
         print("-", num2, "+-", "√(", num2, "^2 - 4(", num1, ")(", num3, "))")
         print("--------------------------------------")
         print('\t' "2(", num1, ")")
